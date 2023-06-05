370d8e4c1f71eb39fa0a859918ec4c83f221bfe3
Chris Patuzzo <chris@patuzzo.co.uk>
2023-06-05 20:20:24 +0100

    Randomise the orientations and redraw frequently

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const image = document.getElementById("source");
 const sourceCanvas = document.getElementById("source-canvas");
-const sourceContext = sourceCanvas.getContext("2d");
-const width = canvas.width;
+const sourceContext = sourceCanvas.getContext("2d"); const width = canvas.width;
 const height = canvas.height;
-const tileSize = 10;
+const tileSize = 5;
 
 sourceContext.drawImage(image, 0, 0, width, height);
 
 const main = () => {
+  setInterval(drawCat, 250);
+};
+
+const drawCat = () => {
+  context.clearRect(0, 0, width, height);
   let t = 0;
 
   for (let y = 0; y < height; y += tileSize) {
@@ -26,7 +30,16 @@ const main = () => {
       const mean = sum / (tileData.length / 4 * 3);
       const t = 1 - (mean / 255);
 
-      drawTileA(x, y, clampT(t));
+      const random = Math.random();
+      if (random < 0.25) {
+        drawTileA(x, y, clampT(t));
+      } else if (random < 0.5) {
+        drawTileB(x, y, clampT(t));
+      } else if (random < 0.75) {
+        drawTileC(x, y, clampT(t));
+      } else {
+        drawTileD(x, y, clampT(t));
+      }
     }
   }
 };
@@ -41,7 +54,36 @@ const drawTileA = (x, y, t) => {
   context.lineTo(x, y + tileSize);
   context.lineTo(x + tileSize, y + tileSize);
   context.lineTo(x + tileSize * t, y + (1 - t) * tileSize);
+  context.closePath();
+  context.fill();
+};
+
+const drawTileB = (x, y, t) => {
+  context.beginPath();
+  context.moveTo(x, y + tileSize);
   context.lineTo(x, y);
+  context.lineTo(x + tileSize, y);
+  context.lineTo(x + tileSize * t, y + t * tileSize);
+  context.closePath();
+  context.fill();
+};
+
+const drawTileC = (x, y, t) => {
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x + tileSize, y);
+  context.lineTo(x + tileSize, y + tileSize);
+  context.lineTo(x + tileSize * (1 - t), y + t * tileSize);
+  context.closePath();
+  context.fill();
+};
+
+const drawTileD = (x, y, t) => {
+  context.beginPath();
+  context.moveTo(x, y + tileSize);
+  context.lineTo(x + tileSize, y + tileSize);
+  context.lineTo(x + tileSize, y);
+  context.lineTo(x + tileSize * (1 - t), y + (1 - t) * tileSize);
   context.closePath();
   context.fill();
 };
