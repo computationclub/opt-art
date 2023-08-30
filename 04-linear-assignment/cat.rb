ab06947d12a5fc0dcfed5c2c67f8cfd2ad206176
Chris Patuzzo <chris@patuzzo.co.uk>
2023-08-30 20:06:53 +0100

    Add the constraint that there can only be one cartoon in each block

--- 04-linear-assignment/cat.rb
+++ 04-linear-assignment/cat.rb
@@ -2,6 +2,7 @@ require "rmagick"
 
 image = Magick::Image.read("cat.jpg").first
 block_size = 25
+num_cartoons = 8
 
 width = image.columns
 height = image.rows
@@ -12,8 +13,15 @@ pencil = Magick::Draw.new
 num_columns = width / block_size
 num_rows = height / block_size
 
-num_rows.times do |block_y|
-  num_columns.times do |block_x|
+total_blocks = num_columns * num_rows
+num_of_each_cartoon = total_blocks / num_columns
+
+puts "There are #{total_blocks} blocks in total."
+puts "And we have #{num_cartoons} cartoons."
+puts "So there will be #{num_of_each_cartoon} of each each cartoon."
+
+brightness_values = num_rows.times.map do |block_y|
+  num_columns.times.map do |block_x|
     sum = 0
 
     (0...block_size).each do |pixel_y|
@@ -29,14 +37,37 @@ num_rows.times do |block_y|
       end
     end
 
-    brightness = sum / (block_size * block_size) / 3
-
-    #pencil.fill("blue")
-    #pencil.rectangle(50, 50, 250, 250)
-    #pencil.draw(mosaic)
-
-    puts "Block #{block_x}, #{block_y} has brightness #{brightness}"
+    sum / (block_size * block_size) / 3
   end
 end
 
-mosaic.write("mosaic.jpg")
+#puts brightness_values.inspect
+
+lp = File.open("mosaic.lp", "w")
+lp.puts "subject to"
+
+puts "There has to be exactly one cartoon in each block:"
+num_rows.times do |y|
+  num_columns.times do |x|
+    terms = []
+
+    num_cartoons.times do |cartoon|
+      terms.push("cartoon_#{cartoon}_y_#{y}_x_#{x}")
+    end
+
+    lp.puts "  #{terms.join(" + ")} == 1"
+  end
+end
+
+lp.close
+
+#left = block_x * block_size
+#top = block_y * block_size
+#right = left + block_size
+#bottom = top + block_size
+#
+#pencil.rectangle(left, top, right, bottom)
+#
+#pencil.draw(mosaic)
+#mosaic.write("mosaic.jpg")
+#
